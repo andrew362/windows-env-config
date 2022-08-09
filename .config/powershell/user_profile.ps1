2fe77c9f05b43cfe1583221f64fdd3ea961eba3e
Andrzej <37003497+andrew362@users.noreply.github.com>
2022-08-09 11:09:26 +0200

    aliases

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,11 +1,20 @@
 #Prompt
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\zash.omp.json" | Invoke-Expression
 
+Function get-gpu { git pull }
+Function get-gma { git checkout master }
+Function get-gc { git checkout - }
+Function get-push { git push }
+
 #Alias
 Set-Alias ll ls -Force
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias -Name gpu -Value get-gpu
+Set-Alias -Name gma -Value get-gma
+Set-Alias -Name gc- -Value get-gc
+Set-Alias -Name push -Value get-push
 
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
